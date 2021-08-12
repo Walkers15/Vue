efcddbcfa3ec9ff533392553c9d2ebe06ab55063
Walkers15 <qortmd7777@gmail.com>
2021-08-13 00:14:18 +0900

    update: Modal

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
 	<div>
+		<div class="black-bg" v-if="isModalOpened === true" @click="isModalOpened = false">
+			<div class="white-bg">
+				<h4>상세페이지</h4>
+				<p>상세페이지내용임</p>
+			</div>
+		</div>
 		<div class="menu">
 			<a v-for="(menuName, i) in menuNames" :key="(menuName, i)">{{ i }}_{{ menuName }}</a>
 		</div>
 
 		<!--<div v-for="(roomName, i) in products" :key="(roomName, i)">
-			<h4 :style="스타일">
+			<h4 :style="style">
 				{{ roomName }}
 			</h4>
 			<p>{{ prices[i] }} 만원</p>
 		</div>-->
 		<div> 
-			<h4>{{ products[0] }}</h4>
+			<img src="https://image.msscdn.net/images/goods_img/20210729/2044024/2044024_1_500.jpg">
+			<h4 @click="isModalOpened = true">
+				{{ products[0] }}
+			</h4>
 			<p>50만원</p>
 			<button @click="increase(reportCounts, '역삼')">
 				허위매물신고
@@ -31,7 +40,7 @@
 				허위매물신고
 			</button> <span> 신고수: {{ reportCounts['마포'] }}</span>
 		</div>
-		<button @click="prices[0] *= 3">
+		<button @click="prices[0] *= 3;">
 			엄준식
 		</button>
 	</div>
@@ -43,10 +52,11 @@ export default {
 	name: "App",
 	data() {
 		return {
+			isModalOpened: false, // ui의 상태 저장: State
 			prices: [60, 70, 80],
 			products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
 			menuNames: ["Home", "Objects", "About"],
-			스타일: "color:red",
+			style: "color:red",
 			reportCounts: {'역삼': 0, '천호': 0, '마포': 0}
 		};
 	},
@@ -78,5 +88,20 @@ export default {
   color : white;
   padding : 10px;
 }
-
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
 </style>
