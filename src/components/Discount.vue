df8922115d14213f42f18a09cc3de04933588f1f
Walkers15 <qortmd7777@gmail.com>
2021-08-17 23:24:20 +0900

    1 마지막

--- src/components/Discount.vue
+++ src/components/Discount.vue
@@ -1,17 +1,29 @@
 <template>
 	<div class="discount" @click="() => {showAlert('엄준식123')}">
-		<h4>지금 결제하면 20% 할인</h4>
+		<h4>지금 결제하면 {{ discountRate }}% 할인</h4>
 	</div>
 </template>
 
 <script>
 export default {
     name: 'Discount',
+	data() {
+		return {
+			discountRate: 30
+		}
+	},
     methods: {
         showAlert: (str) => {
             alert(str);
         }
-    }
+    },
+	mounted() {
+		setInterval(() => {
+			if (this.discountRate > 0) {
+				this.discountRate -= 1;
+			}
+		}, 500)
+	}
 }
 </script>
 
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,7 +4,14 @@
 			<h4>{{ rooms[clickedId].title }}</h4>
 			<img :src="rooms[clickedId].image">
 			<p>{{ rooms[clickedId].content }}</p>
+			<input v-model.number="userInput">
+			<input type="range" min="1" max="12">
 			<p>{{ rooms[clickedId].price }} 원인데...</p>
+			<p> {{ userInput }} 개월 선택함 </p>
+			<p> 그래서 {{ userInput * rooms[clickedId].price }} 원임</p>
+			<textarea v-model="userText" />
+			<p>{{ userText }}</p>
+			<p />
 			<button @click="$emit('discount')">
 				할인 ㄱ?
 			</button>
@@ -20,6 +27,20 @@
 <script>
 export default {
     name: 'Modal',
+	data() {
+		return {
+			userInput: 3,
+			userText: ''
+		}
+	},
+	watch: {
+		userInput(a, b) {
+			if (isNaN(this.userInput) === true) {
+				alert('디질래?')
+				this.userInput = b;
+			}
+		}
+	},
     props: {
 		rooms: {
 			type: Array,
@@ -33,7 +54,13 @@ export default {
 			default: 0
 		}
     },
-	emits: ['closeModal','discount']
+	emits: ['closeModal','discount'],
+	beforeUpdate() {
+		if (this.userInput <= 2) {
+			alert('우리는 두달 이하는 안받아요~')
+			this.userInput = 3;
+		}
+	}
 }
 </script>
 
